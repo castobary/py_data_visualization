4fe647dd6210725e19bf2bf11d3d6d196e88d441
castobary <46651885+castobary@users.noreply.github.com>
2022-05-03 21:26:42 +0300

    Identation is a big deal

--- random_walk.py
+++ random_walk.py
@@ -17,32 +17,30 @@
 from random import choice 
 
 class RandomWalk:
-
-    """A class to generate random walks."""
-    
-def __init__(self, num_points=5000):
-        """Initialize attributes of a walk."""
-        self.num_points = num_points
-        # All walks start at (0, 0).
-        self.x_values = [0]
-        self.y_values = [0]
-
-def fill_walk(self):
-     """Calculate all the points in the walk."""
+        """A class to generate random walks."""
+        def __init__(self, num_points=5000):
+          """Initialize attributes of a walk."""
+          self.num_points = num_points
+# All walks start at (0, 0).
+          self.x_values = [0]
+          self.y_values = [0]
+        def fill_walk(self):
+           """Calculate all the points in the walk."""
+           while len(self.x_values) < self.num_points:
 # Keep taking steps until the walk reaches the desired length.
-     while len(self.x_values) < self.num_points:
+           
 # Decide which direction to go and how far to go in that direction. 
-          x_direction = choice([1, -1]) # choose a value for x_direction, which returns either 1 for right movement or –1 for left
-          x_distance = choice([0, 1, 2, 3, 4]) #tells Python how far to move in that direction (x_distance) by randomly selecting an integer between 0 and 4. 
-          x_step = x_direction * x_distance
-          y_direction = choice([1, -1])
-          y_distance = choice([0, 1, 2, 3, 4])
-          y_step = y_direction * y_distance
+            x_direction = choice([1, -1]) # choose a value for x_direction, which returns either 1 for right movement or –1 for left
+            x_distance = choice([0, 1, 2, 3, 4]) #tells Python how far to move in that direction (x_distance) by randomly selecting an integer between 0 and 4. 
+            x_step = x_direction * x_distance
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
 # Reject moves that go nowhere. ➎ 
-          if x_step == 0 and y_step == 0:
+            if x_step == 0 and y_step == 0:
              continue
 # Calculate the new position. ➏ 
-          x = self.x_values[-1] + x_step
-          y = self.y_values[-1] + y_step
-          self.x_values.append(x)
-          self.y_values.append(y)
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+            self.x_values.append(x)
+            self.y_values.append(y)
