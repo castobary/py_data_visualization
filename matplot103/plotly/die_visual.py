23a801a753c3525a66ee8b703660d4497f55fae7
castobary <46651885+castobary@users.noreply.github.com>
2022-05-04 09:56:50 +0300

    Rolling a die 100 times

---
+++ matplot103/plotly/die_visual.py
@@ -0,0 +1,15 @@
+from die import Die
+
+# Create a D6
+
+die = Die()
+
+# Make some rolls and store the result in a list
+
+results = []
+
+for roll_num in range(100):
+    result = die.roll()
+    results.append(result)
+    
+print(results)
